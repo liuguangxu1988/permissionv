534f1675bd9a37a73b3db89dae98df3b5f9dacec
liuguangxu <gghost2012@163.com>
2023-02-26 18:54:39 +0800

    First Commit.

---
+++ app/src/main/java/com/dante/permissionv/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.dante.permissionv
+
+import android.Manifest
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.dante.library.PermissionV
+import kotlinx.android.synthetic.main.activity_main.*
+
+private const val TAG = "MainActivity"
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        tvCall.setOnClickListener {
+            PermissionV.request(
+                this, Manifest.permission.CALL_PHONE, Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.READ_CONTACTS
+            ) { allGranted, deniedList ->
+                if (allGranted) {
+                    call()
+                    Log.d(TAG, "allGranted")
+                } else {
+                    Toast.makeText(this, "You Denied $deniedList", Toast.LENGTH_SHORT).show()
+                    Log.d(TAG, "Denied")
+                }
+            }
+        }
+    }
+
+    private fun call() {
+        try {
+            val intent = Intent(Intent.ACTION_CALL)
+            intent.data = Uri.parse("tel:10086")
+            startActivity(intent)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+}
